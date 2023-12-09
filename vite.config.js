59a1a360b93fe50436699e9d0aeef833caee6677
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2023-12-09 15:49:30 -0300

    chunks

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'canvas-chunk': ['SpaceShipCanvas', 'BallCanvas', 'ComputersCanvas', 'StarsCanvas'],
-          'app-chunk': ['Hero', 'Navbar', 'About', 'Tech', 'Experience', 'Works', 'Feedbacks', 'Contact', 'CanvasLoader'],
-        },
-      },
-    },
   },
-});
+})
